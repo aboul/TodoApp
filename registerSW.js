a6a69ee0bf8fa71709755b9066341d00788a286d
Abel <abel@earthpicdaily.org>
2024-01-18 11:16:49 +0100

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/TodoApp/sw.js', { scope: '/TodoApp/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
